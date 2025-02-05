23df5b90b79f19d4f90c557c44a82c13482fe8ab
Tiansi Gu <tgu10@dons.usfca.edu>
2025-02-05 14:37:55 -0800

    migrate retries to docker-compose

--- db_init.sql
+++ db_init.sql
@@ -1,9 +1,9 @@
 USE test;
 
 CREATE TABLE books (
-    id INT PRIMARY KEY AUTO_INCREMENT,  -- Primary key, unique and not null
-    title VARCHAR(255) NOT NULL,         -- Title must not be null
-    description TEXT NOT NULL,           -- Description must not be null
-    cover VARCHAR(255) NULL,             -- Cover can be null (nullable field)
-    price DECIMAL(10,2) NOT NULL         -- Price must not be null
+    id INT PRIMARY KEY AUTO_INCREMENT,  
+    title VARCHAR(255) NOT NULL,       
+    description TEXT NOT NULL,          
+    cover VARCHAR(255) NULL,            
+    price DECIMAL(10,2) NOT NULL 
 );
